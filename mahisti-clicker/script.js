365012be5c51fcc6bf7e4116e026935d15f0de3f
stbow <sab805@gmail.com>
2025-02-26 15:01:40 -0700

    v1.1 updated costs/revenues values

--- mahisti-clicker/script.js
+++ mahisti-clicker/script.js
@@ -16,26 +16,34 @@ const shopCost = document.getElementById("shop-cost");
 const fleetDiv = document.getElementById("fleet-div");
 const fleetCount = document.getElementById("fleet-count");
 const fleetCost = document.getElementById("fleet-cost");
+const minesDiv = document.getElementById("mines-div");
+const minesCount = document.getElementById("mines-count");
+const minesCost = document.getElementById("mines-cost");
 const sell = document.getElementById("sell");
 const hire = document.getElementById("hire");
 const buyShop = document.getElementById("buy-shop");
 const buyShip = document.getElementById("buy-ship");
+const buyMine = document.getElementById("buy-mine");
 
 var balance = 0;
 var employees = 0;
 var shops = 0;
 var ships = 0;
+var mines = 0;
 var nextEmployee = 5;
-var nextShop = 1000;
-var nextShip = 100000;
+var nextShop = 1080;
+var nextShip = 99000;
+var nextMine = 9000000;
 var empMult = 1;
 var shopsMult = 120;
-var fleetMult = 15000;
+var fleetMult = 14400;
+var minesMult = 1800000;
 
 sell.onclick = sellItem;
 hire.onclick = hireEmployee;
 buyShop.onclick = newShop;
 buyShip.onclick = newShip;
+buyMine.onclick = newMine;
 
 function sellItem() {
   balance++;
@@ -61,7 +69,7 @@ function newShop() {
     balance -= nextShop;
     balanceText.innerText = balance;
     shopsCount.innerText = shops;
-    nextShop = Math.floor(1000 * Math.pow(1.1,shops));
+    nextShop = Math.floor(1080 * Math.pow(1.1,shops));
     shopCost.innerText = nextShop;
     convertCurrency(balance);
   }
@@ -73,8 +81,20 @@ function newShip() {
     balance -= nextShip;
     balanceText.innerText = balance;
     fleetCount.innerText = ships;
-    nextShip = Math.floor(100000 * Math.pow(1.1,ships));
+    nextShip = Math.floor(99000 * Math.pow(1.1,ships));
     fleetCost.innerText = nextShip;
+    convertCurrency(balance);
+  }
+}
+
+function newMine() {
+  if (balance >= nextMine) {
+    mines++;
+    balance -= nextMine;
+    balanceText.innerText = balance;
+    minesCount.innerText = mines;
+    nextMine = Math.floor(9000000 * Math.pow(1.1,mines));
+    minesCost.innerText = nextMine;
     convertCurrency(balance);
   }
 }
